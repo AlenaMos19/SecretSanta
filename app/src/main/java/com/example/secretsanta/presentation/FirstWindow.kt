7f6249a835781f6bb9ce95564688bc572fc13e1e
AlenaMos19 <am79819712119@gmail.com>
2022-11-10 16:07:03 +0300

    create new and removed unnecessary activity

---
+++ app/src/main/java/com/example/secretsanta/presentation/FirstWindow.kt
@@ -0,0 +1,13 @@
+package com.example.secretsanta.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.secretsanta.R
+//import com.example.secretsanta.databinding.FragmentFirstBinding
+
+class FirstWindow : Fragment() {}
+
